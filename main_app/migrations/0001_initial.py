af80cee2a823ba38aa33811bb4ef80910c160691
aidanbell0 <aidanbell0182@gmail.com>
2020-02-24 10:40:19 -0500

    add requirements.txt

--- main_app/migrations/0001_initial.py
+++ main_app/migrations/0001_initial.py
@@ -12,49 +12,60 @@ class Migration(migrations.Migration):
     dependencies = [
         ('auth', '0011_update_proxy_permissions'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('auth', '0011_update_proxy_permissions'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Attendant',
             fields=[
-                ('date', models.DateField(auto_now=True, verbose_name='confirmation date')),
-                ('confirmation', models.CharField(choices=[('Y', 'Yes'), ('N', 'No'), ('M', 'Maybe')], default='Y', max_length=1)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('date', models.DateField(auto_now=True,
+                                          verbose_name='confirmation date')),
+                ('confirmation', models.CharField(choices=[
+                 ('Y', 'Yes'), ('N', 'No'), ('M', 'Maybe')], default='Y', max_length=1)),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE,
+                                              primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='Profile',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True, serialize=False, verbose_name='ID')),
                 ('phone', models.CharField(blank=True, max_length=10, null=True)),
                 ('location', models.CharField(blank=True, max_length=50, null=True)),
                 ('birthday', models.DateField(blank=True, null=True)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('user', models.OneToOneField(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='Event',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
                 ('location', models.CharField(max_length=100)),
                 ('address', models.TextField(max_length=250)),
                 ('date_time', models.DateTimeField()),
                 ('occasion', models.TextField(max_length=150)),
-                ('attendants', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='main_app.Attendant')),
-                ('creator', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('creator', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='Comment',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True, serialize=False, verbose_name='ID')),
                 ('content', models.TextField(max_length=150)),
                 ('created', models.DateTimeField(auto_now=True)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-                ('event_id', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='main_app.Event')),
-                ('parent', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='main_app.Comment')),
+                ('author', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('event_id', models.ForeignKey(
+                    on_delete=django.db.models.deletion.CASCADE, to='main_app.Event')),
+                ('parent', models.ForeignKey(blank=True, null=True,
+                                             on_delete=django.db.models.deletion.CASCADE, to='main_app.Comment')),
             ],
         ),
     ]
